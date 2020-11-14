3a942328772352f8bedab388e9cf89abe062d02a
Husanjeet <husanjeetkaur1996@gmail.com>
2020-11-14 16:41:38 -0500

    signup changes

--- app/src/main/java/com/example/giftproject/SignUp.java
+++ app/src/main/java/com/example/giftproject/SignUp.java
@@ -1,8 +1,5 @@
 package com.example.giftproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -13,6 +10,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,7 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SignUp extends AppCompatActivity {
 
     EditText Name,Email,Password;
-    Button Signup;
+    Button register;
     TextView link;
     FirebaseAuth auth;
     ProgressBar progressBar;
@@ -35,7 +35,8 @@ public class SignUp extends AppCompatActivity {
         Email = findViewById(R.id.Email);
         Password = findViewById(R.id.Password);
 
-        Signup = findViewById(R.id.SignUp);
+        register = findViewById(R.id.SignUp);
+
         link = findViewById(R.id.LoginLink);
         progressBar = findViewById(R.id.progressBar);
 
@@ -47,58 +48,47 @@ public class SignUp extends AppCompatActivity {
 
         }
 
-        Signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = Email.getText().toString().trim();
-                String password = Password.getText().toString().trim();
+        register.setOnClickListener(v -> {
+            String email = Email.getText().toString().trim();
+            String password = Password.getText().toString().trim();
 
-                if(TextUtils.isEmpty(email)){
+            if(TextUtils.isEmpty(email)){
 
-                    Email.setError("Email is required");
-                    return;
+                Email.setError("Email is required");
+                return;
 
-                }
-                if(TextUtils.isEmpty(password)){
-
-                    Password.setError("Password is required");
-                    return;
-
-                }
-                if(password.length()<6){
-
-                    Password.setError("Password must contain six characters");
-                    return;
-
-                }
-                progressBar.setVisibility(View.VISIBLE);
-
-                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if(task.isSuccessful()){
-                            Toast.makeText(SignUp.this,"Sign Up successfully",Toast.LENGTH_SHORT).show();
-
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }else {
-                            Toast.makeText(SignUp.this,"Error!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.GONE);
-
-                        }
-
-
-                    }
-                });
             }
+            if(TextUtils.isEmpty(password)){
+
+                Password.setError("Password is required");
+                return;
+
+            }
+            if(password.length()<6){
+
+                Password.setError("Password must contain six characters");
+                return;
+
+            }
+            progressBar.setVisibility(View.VISIBLE);
+
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUp.this,"Sign Up successfully",Toast.LENGTH_LONG).show();
+
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                }else {
+                    Toast.makeText(SignUp.this,"Error!"+task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+
+                }
+            });
+
         });
 
-        link.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            }
-        });
+
+        link.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),MainActivity.class)));
 
     }
 }
