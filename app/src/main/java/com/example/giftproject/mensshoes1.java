5f3dbdb8c4c6e915c54494bfcebbb37cece6ef15
Jageergill <jageer.gill@gmail.com>
2020-11-21 23:33:09 -0800

    Mens Shoes Grid Java

--- app/src/main/java/com/example/giftproject/mensshoes1.java
+++ app/src/main/java/com/example/giftproject/mensshoes1.java
@@ -2,13 +2,37 @@ package com.example.giftproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class mensshoes1 extends AppCompatActivity {
 
+    TextView returnHome;
+    TextView returnProducts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensshoes1);
+
+        returnHome=findViewById(R.id.home_btn);
+        returnProducts=findViewById(R.id.products_btn);
+
+        returnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        returnProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Menshome.class);
+                startActivity(i);
+            }
+        });
     }
 }
