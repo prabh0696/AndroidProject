e1b138b596229c240a9c2825494a0dd4ff5563c6
Jageergill <jageer.gill@gmail.com>
2020-11-22 00:09:37 -0800

    Login Type Activity

---
+++ app/src/main/java/com/example/giftproject/loginType.java
@@ -0,0 +1,14 @@
+package com.example.giftproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_type);
+    }
+}
