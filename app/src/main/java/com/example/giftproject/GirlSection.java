6e727c6500fb277ef004622191ae8d465724b541
Husanjeet <husanjeetkaur1996@gmail.com>
2020-11-21 21:59:17 -0500

    kids section

--- app/src/main/java/com/example/giftproject/GirlSection.java
+++ app/src/main/java/com/example/giftproject/GirlSection.java
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link GirlSection#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class GirlSection extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +24,7 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    public GirlSection() {
         // Required empty public constructor
     }
 
@@ -35,11 +34,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment GirlSection.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static GirlSection newInstance(String param1, String param2) {
+        GirlSection fragment = new GirlSection();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,29 +58,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
-        final View hid= inflater.inflate(R.layout.fragment_home, container, false);
-        ImageView menbtn=(ImageView) hid.findViewById(R.id.menhome);
-        ImageView kidSec=(ImageView) hid.findViewById(R.id.kidhome);
-
-        menbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new Menshome()).commit();
-            }
-        });
-
-
-        kidSec.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new KidsSection()).commit();
-            }
-        });
-        return hid;
+        return inflater.inflate(R.layout.fragment_girl_section, container, false);
     }
 }
--- app/src/main/java/com/example/giftproject/SignUp.java
+++ app/src/main/java/com/example/giftproject/SignUp.java
@@ -20,11 +20,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText Name,Email,Password;
-    Button register;
-    TextView link;
-    FirebaseAuth auth;
-    ProgressBar progressBar;
+    private EditText Name,Email,Password;
+    private Button register;
+   private TextView link;
+   private FirebaseAuth auth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,10 +82,11 @@ public class SignUp extends AppCompatActivity {
             });
 
         });
-
-
-        link.setOnClickListener(v -> {
-            startActivity(new Intent(getApplicationContext(), LoginPage.class));
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            }
         });
 
     }
