d5d19e93ef0326e2ebe93f3647f4650a0647123d
Jageergill <jageer.gill@gmail.com>
2020-12-11 21:15:05 -0800

    Minor Changes

--- app/src/main/java/com/example/giftproject/RetrofitFeedbackShow.java
+++ app/src/main/java/com/example/giftproject/RetrofitFeedbackShow.java
@@ -1,14 +1,67 @@
 package com.example.giftproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.giftproject.api_interfaces.MyJsonApi;
+import com.example.giftproject.models.UserFeedback;
+
+import java.util.List;
 
 public class RetrofitFeedbackShow extends AppCompatActivity {
 
+    TextView textviewresult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrofit_feedback_show);
+
+        textviewresult=findViewById(R.id.text_view_result);
+
+        getFeedbacks();
+    }
+
+    public void getFeedbacks(){
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MyJsonApi myJsonApi=retrofit.create(MyJsonApi.class);
+        Call<List<UserFeedback>> call=myJsonApi.getFeedbacks();
+
+        call.enqueue(new Callback<List<UserFeedback>>() {
+            @Override
+            public void onResponse(Call<List<UserFeedback>> call, Response<List<UserFeedback>> response) {
+                if(!response.isSuccessful()){
+                    textviewresult.setText("Code: "+response.code());
+                }
+
+                List<UserFeedback> userFeedbacks=response.body();
+                for(UserFeedback userFeedback:userFeedbacks){
+                    String content="";
+                    content += "ID: "+userFeedback.getId()+"\n";
+                    content += "Name: "+userFeedback.getName()+"\n";
+                    content += "Email: "+userFeedback.getEmail()+"\n";
+                    content += "Phone: "+userFeedback.getPhone()+"\n";
+                    content += "Product Type: "+userFeedback.getProducttype()+"\n";
+                    content += "Feedback: "+userFeedback.getUserfeedback()+"\n\n";
+
+                    textviewresult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<UserFeedback>> call, Throwable t) {
+                textviewresult.setText(t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/giftproject/models/UserFeedback.java
+++ app/src/main/java/com/example/giftproject/models/UserFeedback.java
@@ -1,4 +1,70 @@
 package com.example.giftproject.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class UserFeedback {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String producttype;
+
+    @SerializedName("feedback")
+    private String userfeedback;
+
+    public UserFeedback(String name, String email, String phone, String producttype, String userfeedback) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.producttype = producttype;
+        this.userfeedback = userfeedback;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProducttype() {
+        return producttype;
+    }
+
+    public void setProducttype(String producttype) {
+        this.producttype = producttype;
+    }
+
+    public String getUserfeedback() {
+        return userfeedback;
+    }
+
+    public void setUserfeedback(String userfeedback) {
+        this.userfeedback = userfeedback;
+    }
 }
